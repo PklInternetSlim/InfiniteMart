e69ca9ed35dbb30cc7db1e377a2d8d05fc6f00dd
Rqwannn <mohwannn1@gmail.com>
2021-03-01 00:06:18 +0700

    Menyelesaikan Admin

---
+++ Assets/Js/PembayaranGagal.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#TableOrder").DataTable({
+        paging: true,
+        lengthChange: false,
+        searching: true,
+        ordering: true,
+        info: true,
+        autoWidth: false,
+        responsive: true,
+    });
+});
